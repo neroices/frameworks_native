9252c8d25388ee5337859085c867cc30d2921c1a
Jesse Chan <jc@lineageos.org>
2020-11-02 18:53:54 +0100

    SF: Allow force fallback to Light HAL for brightness
    In commit "Moved brightness from Lights to SF.", it becomes
    possible to implement brightness functions in HW Composer HAL
    instead of Light HAL.
    
    However, some vendors implement this in a non-conforming way
    which leads to undesired results.
    
    Re-implementing HW Composer HAL is extremely hard if not impossible
    for some devices while Light HAL can be easily re-implemented. Thus,
    improper implementations of brightness functions cannot be overridden
    easily if they are in HW Composer HAL.
    
    This change adds a property "ro.sf.force_light_brightness" to allow
    force fallback to Light HAL for brightness functions.
    
    Change-Id: I063de09e7ebbd75b4dc36ca0ae9914ee408e7447
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1421,6 +1421,7 @@ private:
     scheduler::PresentLatencyTracker mPresentLatencyTracker GUARDED_BY(kMainThreadContext);
 
     bool mLumaSampling = true;
+    bool mForceLightBrightness = false;
     sp<RegionSamplingThread> mRegionSamplingThread;
     sp<FpsReporter> mFpsReporter;
     sp<TunnelModeEnabledReporter> mTunnelModeEnabledReporter;
